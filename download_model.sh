23d87b0d2af5e774b6918d5977f55850d367b0be
Minsub Won <minsubwon@naver.com>
2020-01-15 21:29:55 +0900

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -7,5 +7,5 @@ tar -zxvf $FILENAME
 rm $FILENAME
 
 FILEID=1se1bXiSuebpphOCoZWB5MWsTShwUAsIo
-
+FILENAME=word2idx.pkl
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$FILEID'' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
