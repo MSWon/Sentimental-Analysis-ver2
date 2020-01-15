6eca68aad12cb33cc78a8397eb063d10c8d1db44
Minsub Won <minsubwon@naver.com>
2020-01-15 21:43:41 +0900

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -14,15 +14,15 @@ if __name__ == '__main__':
     FLAGS = flags.FLAGS
     
     ## Model parameter
-    flags.DEFINE_integer('batch_size', 128, 'number of batch size')
+    flags.DEFINE_integer('batch_size', 1, 'number of batch size')
     flags.DEFINE_integer('word_dim', 512, 'dimension of word vector')
     flags.DEFINE_integer('hidden_dim', 512, 'dimension of model hidden nodes')
-    flags.DEFINE_integer('num_layers', 3, 'number of LSTM layers')
+    flags.DEFINE_integer('num_layers', 2, 'number of LSTM layers')
     flags.DEFINE_integer('max_vocab_size', 100000, 'max vocabulary size')
     flags.DEFINE_integer('max_word_len', 100, 'max length of words')
     flags.DEFINE_float('learning_rate', 0.0001, 'initial learning rate')
     flags.DEFINE_string('path', 'Movie_rating_data', 'directory of data file')
-    flags.DEFINE_integer('training_epochs', 10, 'number of training epochs')
+    flags.DEFINE_integer('training_epochs', 5, 'number of training epochs')
     
     print('========================')
     for key in FLAGS.__flags.keys():
