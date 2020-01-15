b8d6d500e2ff7cae9804e5bca4861f66e74f6edb
Minsub Won <minsubwon@naver.com>
2020-01-15 21:44:33 +0900

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -1,4 +1,4 @@
-FILEID=1gMmRS5EKIL9MJgMkMFJHbD6ZnkYUGKY6
+FILEID=12Nh7U6ton4JB5K5DqOBCclpCQwsj-x5G
 FILENAME=sent_model.tar.gz
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$FILEID'' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
